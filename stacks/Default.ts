6bd9ff2907ee27ccce21eb91a60d4134fd202667
Sagar Chavan <sagar.c.03@gmail.com>
2023-07-14 16:05:32 -0400

    add(database): add dataabase config to default stack

--- stacks/Default.ts
+++ stacks/Default.ts
@@ -1,10 +1,16 @@
-import { StackContext, NextjsSite } from "sst/constructs";
+import { StackContext, NextjsSite, RDS } from "sst/constructs";
 
 export function Default({ stack }: StackContext) {
+  const rds = new RDS(stack, "database", {
+    engine: "postgresql13.9",
+    defaultDatabaseName: "db",
+    migrations: "packages/core/migration",
+  });
   const site = new NextjsSite(stack, "site", {
     path: "packages/web",
   });
   stack.addOutputs({
     SiteUrl: site.url,
+    ClusterArn: rds.clusterArn,
   });
 }
